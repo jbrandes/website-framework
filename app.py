060602228ea5bbedc6b6fd478888b8e13797c526
Jordan Brandes <jordanbrandes@yahoo.com>
2019-08-07 18:43:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def hello():
+    return "Welcome to the server"
+
 @app.route('/home')
 def home():
     return render_template("home.html")
+
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
